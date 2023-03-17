1e37eed4e869f0d3565bc85ab064ba81efe5f130
kousikkgk <kousikup23@gmail.com>
2023-03-17 10:48:37 +0530

    refs#Spring cloud example updated

---
+++ insurance-provider/src/test/java/com/insurance/provider/InsuranceProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insurance.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
